c401b43b423e69c7ea097cb0d7f5bbec5dc9af17
Noemie.BARRE <noemie.barre24@gmail.com>
2023-04-01 16:38:20 +0200

    register

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../src/hello.php';
+
+$result = new Hello;
+echo $result->talk();
+
+?>
---
+++ src/hello.php
@@ -0,0 +1,12 @@
+ <?php
+
+ class Hello
+{
+    public function talk() : string
+    {
+        return 'Hello world !';
+    }
+}
+
+
+?>
